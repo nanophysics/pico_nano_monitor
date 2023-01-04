7909d93e8568e3579029a7200a85930fbb224a81
peter.maerki <git.peter@ergoinfo.ch>
2023-01-04 11:59:15 +0100

    .

--- uniq_id_names.py
+++ uniq_id_names.py
@@ -1,4 +1,4 @@
-# unic id of the board and names written by tape on the circuit board
+# unic id of the board and names written on label on the circuit board
 
 names_id_dict = {
 b'e6616407e3578c2c': 'pico_emil',
