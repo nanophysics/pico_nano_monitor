f48fad96fc3912d7ec7a03208917f0e7443af40b
peter.maerki <git.peter@ergoinfo.ch>
2023-01-24 22:48:45 +0100

    .

--- uniq_id_names.py
+++ uniq_id_names.py
@@ -5,5 +5,5 @@ b'e6616407e3578c2c': dict(name='pico_emil', src='src/pico_emil'),
 b'e6614104034a572f': dict(name='pico_hugo', src='src/pico_hugo'),
 b'e6616407e3115431': dict(name='pico_susi', src='src/pico_susi'),
 b'e661640843604922': dict(name='pico_heinz', src='src/pico_heinz'),
-b'e661640843866028': dict(name='pico_rahel', src='src/pico_rahel'),
+b'e661640843866028': dict(name='pico_rahel', src='src/vibration_temperature'),
 }
