b43e0688fe449cfb532dad730e1d6292627bd14b
peter.maerki <git.peter@ergoinfo.ch>
2023-07-14 14:28:35 +0200

    .

--- src/pico_rosa/main.py
+++ src/pico_rosa/main.py
@@ -103,7 +103,7 @@ outage_actual = Outage( trace_array_len = trace_array_len )
 outage_report = Outage()
 outage_upload = Outage( trace_array_len = trace_array_len)
 
-adc26 = machine.ADC(machine.Pin("GPIO28"))
+adc26 = machine.ADC(machine.Pin("GPIO26"))
 
 baton = _thread.allocate_lock()
 
