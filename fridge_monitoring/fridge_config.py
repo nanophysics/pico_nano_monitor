c416250687a19b5b07cc617924b09256af2e36fa
benedikt <benekrat@phys.ethz.ch>
2024-01-23 18:27:40 -0800

    Port to Tabea

--- fridge_monitoring/fridge_config.py
+++
@@ -1,3 +0,0 @@
-import config_sofia as config 
-
-get_fridge = config.get_fridge
--- fridge_monitoring/main.py
+++ fridge_monitoring/main.py
@@ -1,7 +1,34 @@
-import fridge_config 
+import os
+import time
 from watch_dog import OnMyWatch
+from utils import BlueforsFridge
 
-fridge = fridge_config.get_fridge()
-wd = OnMyWatch(fridge)
-if __name__ == "__main__": 
-    wd.run()
+
+def get_fridge() -> BlueforsFridge:
+    username = os.environ["USERNAME"]
+
+    fridge_name = {
+        "Sofia_CryoPC": "sofia",
+        "Tabea_CryoPC": "tabea",
+    }[username]
+
+    return BlueforsFridge(
+        logging_device=f"bluefors_{fridge_name}",
+        setup=fridge_name,
+        room="B17",
+        user="pmaerki",
+        log_folder=rf"C:\Users\{username}\Bluefors logs",
+        manufacturer="Bluefors",
+    )
+
+
+fridge = get_fridge()
+if __name__ == "__main__":
+    while True:
+        try:
+            wd = OnMyWatch(fridge)
+            wd.run()
+        except Exception as e:
+            print(e)
+            print("Restarting in 10s")
+            time.sleep(10.0)
